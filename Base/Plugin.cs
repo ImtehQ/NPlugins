c60ceb5f77c951d418bb0ea3f9359d105a3aa3e5
ImtehQ <its_q@hotmail.com>
2021-06-24 15:05:46 +0200

    Add project files.

---
+++ Base/Plugin.cs
@@ -0,0 +1,19 @@
+﻿using MissingUnityTypes;
+using NPlugins.Interfaces;
+using System;
+
+namespace NPlugins.Base
+{
+    public class Plugin : IPlugin
+    {
+        public virtual float GetWaterLevel(Vector3 pos)
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual void Initialize()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Interfaces/IPlugin.cs
@@ -0,0 +1,14 @@
+﻿using MissingUnityTypes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NPlugins.Interfaces
+{
+    interface IPlugin
+    {
+        void Initialize();
+
+        float GetWaterLevel(Vector3 pos);
+    }
+}
